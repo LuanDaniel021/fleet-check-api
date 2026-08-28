43cde23b28730171535c13f8cfd4410edbcda33e
LuanDaniel021 <luancordeiro021@gmail.com>
2026-08-28 17:22:04 -0300

    fix: BearerAuth type https

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap(): Promise<void> {
     .setVersion('1.0')
     .addBearerAuth(
       {
-        type: 'http',
+        type: 'https',
         scheme: 'bearer',
         bearerFormat: 'JWT',
         name: 'JWT',
