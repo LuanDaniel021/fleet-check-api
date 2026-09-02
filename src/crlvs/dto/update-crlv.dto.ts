b213311d95c27a249e7639b44003d514145f83c5
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-02 15:35:57 -0300

    refactor: Update UpdateCrlvDto to use IUpdateDto interface

--- src/crlvs/dto/update-crlv.dto.ts
+++ src/crlvs/dto/update-crlv.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCrlvDto } from './create-crlv.dto';
+import { IUpdateDto } from '../../supabase/supabase.interfaces';
 
-export class UpdateCrlvDto extends PartialType(CreateCrlvDto) {}
+export class UpdateCrlvDto extends PartialType(CreateCrlvDto) implements IUpdateDto<'crlv'> {}
